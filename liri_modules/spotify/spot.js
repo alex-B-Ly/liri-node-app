adf508f65d8bce954bee26e5d9db609149e7e758
Alex Ly <alexly@Alexs-MBP.home>
2016-01-19 14:28:45 -0500

    added if statement to check if song is entered.

--- liri_modules/spotify/spot.js
+++ liri_modules/spotify/spot.js
@@ -1,5 +1,13 @@
 // TODO install npm spotify package and make require var here
 
 exports.spotifyLogic = function(){
-  console.log('spotify works');
+  var argv = process.argv.slice(2);
+
+  if(argv[1]){
+    // TODO Build out song logic to return info
+    console.log('song chosen: '+ argv[1]);
+  }else{
+    // TODO Default song will go here
+    console.log('please choose a song.');
+  }
 }
