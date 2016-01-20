c51fd362b679f32e8e411d3f0f262dd02e4dcac7
Alex Ly <alexly@Alexs-MBP.home>
2016-01-20 18:30:04 -0500

    omdb functionality is working.  Need to clean things up a bit by making console logs into vars to be passed to log.txt.

---
+++ liri_modules/omdb/omdb.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var argv = process.argv.slice(3);
+
+
+exports.omdbLogic = function(){
+  var userMovie = argv[0];
+
+  function movieChoose(movie){
+    request('http://www.omdbapi.com/?t='+movie+'&tomatoes=true', function(error, response, body){
+      var infoDestringed = JSON.parse(body);
+
+      var movieInfo = 'Title: '+ infoDestringed.Title + '\r\n'
+      +'Year: ' + infoDestringed.Year + '\r\n' 
+      +'Imdb rating: ' + infoDestringed.imdbRating + '\r\n'
+      +'Country: ' + infoDestringed.Country + '\r\n'
+      +'Language: ' + infoDestringed.Language + '\r\n'
+      +'Plot: ' + infoDestringed.Plot + '\r\n'
+      +'Actors: ' + infoDestringed.Actors + '\r\n';
+
+      // TODO Add Rotten Tomatoes rating and url
+
+      if(error){
+        console.log('error type: '+error);
+      }else{
+       console.log(movieInfo);
+      }
+    });
+  }
+
+  if(userMovie){
+    movieChoose(userMovie);
+  }else{
+    movieChoose('star wars');
+  }
+
+
+}
