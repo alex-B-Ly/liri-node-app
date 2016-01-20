386cd9b9ccaae885f0ebad7650f8d8423163f2a2
Alex Ly <alexly@Alexs-MBP.home>
2016-01-20 12:55:14 -0500

    Built out basic spotify functionality.

--- liri_modules/spotify/spot.js
+++ liri_modules/spotify/spot.js
@@ -1,13 +1,37 @@
-// TODO install npm spotify package and make require var here
+var spot = require('spotify');
 
 exports.spotifyLogic = function(){
-  var argv = process.argv.slice(2);
+  var argv = process.argv.slice(3);
 
-  if(argv[1]){
-    // TODO Build out song logic to return info
-    console.log('song chosen: '+ argv[1]);
+  if(argv[0]){
+    // Looks for song name passed by user and returns song info
+    spot.search({ type: 'track', query: argv[0]}, function(err, data){
+      if(err){
+        console.log('error type: '+err);
+      }else{
+        // TODO Use higher order functions to pass info from each returned item in the JSON data
+          // TODO Make the console log strings into one variable
+        console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+        console.log('Song: ' + data.tracks.items[0].name);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('Preview link: ' + data.tracks.items[0].preview_url);
+        console.log('\r\n');
+      }
+    });
+
   }else{
-    // TODO Default song will go here
-    console.log('please choose a song.');
+    // Default song goes here if no arguments are passed by user
+    spot.search({ type: 'track', query: "what's my age again?"}, function(err, data){
+      if(err){
+        console.log('error type: '+err);
+      }else{
+        console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+        console.log('Song: ' + data.tracks.items[0].name);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('Preview link: ' + data.tracks.items[0].preview_url);
+        console.log('\r\n'); 
+      }
+
+    });
   }
 }
