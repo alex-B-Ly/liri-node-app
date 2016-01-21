1ea921b92697b2099ada098808358d38f20b83be
Alex Ly <alexly@Alexs-MBP.home>
2016-01-20 19:00:22 -0500

    Added tomatoes to movies.

--- liri_modules/omdb/omdb.js
+++ liri_modules/omdb/omdb.js
@@ -9,20 +9,21 @@ exports.omdbLogic = function(){
     request('http://www.omdbapi.com/?t='+movie+'&tomatoes=true', function(error, response, body){
       var infoDestringed = JSON.parse(body);
 
+      // movieInfo returns all relevant info
       var movieInfo = 'Title: '+ infoDestringed.Title + '\r\n'
       +'Year: ' + infoDestringed.Year + '\r\n' 
       +'Imdb rating: ' + infoDestringed.imdbRating + '\r\n'
       +'Country: ' + infoDestringed.Country + '\r\n'
       +'Language: ' + infoDestringed.Language + '\r\n'
       +'Plot: ' + infoDestringed.Plot + '\r\n'
-      +'Actors: ' + infoDestringed.Actors + '\r\n';
-
-      // TODO Add Rotten Tomatoes rating and url
+      +'Actors: ' + infoDestringed.Actors + '\r\n'
+      +'Tomato Rating: ' + infoDestringed.tomatoRating + '\r\n'
+      +'Tomato url: ' + infoDestringed.tomatoURL + '\r\n';
 
       if(error){
         console.log('error type: '+error);
       }else{
-       console.log(movieInfo);
+        console.log(movieInfo);
       }
     });
   }
